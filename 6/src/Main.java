32699cca23440e7d27c39d45b91694ae82287a66
Damgom <wisewish1@gmail.com>
2022-07-06 20:52:35 +0900

    study

---
+++ 6/src/Main.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+// 백준 10926 ??!
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        // id값을 Scanner로 입력받아 할당한 후
+        // "??!"를 붙여 출력
+        String id = sc.next();
+        System.out.println(id + "??!");
+    }
+}
+
