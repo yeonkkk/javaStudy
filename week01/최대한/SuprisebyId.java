bd4bad0d2f0b05c259f94252112afec9dcef98db
daihan-ch22 <daehan.dan.choi@gmail.com>
2022-07-08 18:39:57 +0900

    6/6

---
+++ week01/최대한/SuprisebyId.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class SuprisebyId {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String id = sc.nextLine();
+        sc.close();
+        System.out.println(id + "??!");
+    }
+}
