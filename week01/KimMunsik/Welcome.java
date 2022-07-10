2c738c50fbb3fab15b1a7644c42ecd850b63b932
kimmunsik <munsik419@gmail.com>
2022-07-10 18:29:53 +0900

    To create a folder of KimMunsik

---
+++ week01/KimMunsik/Welcome.java
@@ -0,0 +1,7 @@
+class Welcome {
+    public static void main(String[]args){
+        System.out.println(".  .   .");
+        System.out.println("|  | _ | _. _ ._ _  _");
+        System.out.println("|/\\|(/.|(_.(_)[ | )(/.");
+    }
+}
--- week01/Welcome.java
+++
@@ -1,7 +0,0 @@
-class Welcome {
-    public static void main(String[]args){
-        System.out.println(".  .   .");
-        System.out.println("|  | _ | _. _ ._ _  _");
-        System.out.println("|/\\|(/.|(_.(_)[ | )(/.");
-    }
-}
