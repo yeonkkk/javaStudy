6b346626a2a698ef0f0d299be2607ff5a3b50009
munsik419 <104332972+munsik419@users.noreply.github.com>
2022-07-11 21:44:57 +0900

    Add files via upload
    6개 과제 풀이 완료

--- week01/KimMunsik/Welcome.java
+++ week01/KimMunsik/Welcome.java
@@ -1,5 +1,5 @@
-class Welcome {
-    public static void main(String[]args){
+public class Welcome {
+    public static void main(String[] args) {
         System.out.println(".  .   .");
         System.out.println("|  | _ | _. _ ._ _  _");
         System.out.println("|/\\|(/.|(_.(_)[ | )(/.");
