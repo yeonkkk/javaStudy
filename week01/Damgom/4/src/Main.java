c27113a40ad8048f9cb1267c6a5929afff09a17b
Damgom <wisewish1@gmail.com>
2022-07-12 18:42:58 +0900

    javaStudy week01

---
+++ week01/Damgom/4/src/Main.java
@@ -0,0 +1,12 @@
+
+//백준 5337번 웰컴
+public class Main {
+    public static void main(String[] args) {
+        // 줄바꿈 \n
+        // \을 문자로 인식하기 위해서는 앞에 \를 하나 더 해줘야한다
+        // 관련자료 escape sequence
+        System.out.println(".  .   .\n"
+                + "|  | _ | _. _ ._ _  _\n"
+                + "|/\\|(/.|(_.(_)[ | )(/.\n");
+    }
+}
