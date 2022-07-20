f545d2c1f4c7af6797bc3ee85add1dec5cfecade
devEdok13 <dev.edok13@gmail.com>
2022-07-20 17:43:54 +0900

    220720_BOJ_3Week_init

--- week01/조진우/baekjoon5337.java
+++
@@ -1,7 +0,0 @@
-public class baekjoon5337 {
-    public static void main(String[] args) {
-        System.out.println(".  .   .");
-        System.out.println("|  | _ | _. _ ._ _  _");
-        System.out.println("|/\\|(/.|(_.(_)[ | )(/.");
-    }
-}
