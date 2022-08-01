522a4d0c8124423c18b49c0f07c601c96066f15f
devEdok13 <dev.edok13@gmail.com>
2022-08-01 22:25:34 +0900

    220801_BOJ_4WEEK_Done(1712,10039,10797)

---
+++ week04/이동관/BOJ_1712.java
@@ -0,0 +1,22 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class BOJ_ {
+  public static void main(String[] args) throws IOException {
+  // I/O Set-up
+    BufferedReader br =
+        new BufferedReader(new InputStreamReader(System.in));
+    BufferedWriter bw =
+        new BufferedWriter(new OutputStreamWriter(System.out));
+
+    StringTokenizer st = new StringTokenizer(br.readLine());
+    StringBuilder sb = new StringBuilder();
+
+  // var-init
+
+  // Calculate
+
+  // Out-put
+
+  }
+}
