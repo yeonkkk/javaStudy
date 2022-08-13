e7e43a864279286335e6d123ae23d55fa10130b0
devEdok13 <dev.edok13@gmail.com>
2022-08-13 23:12:19 +0900

    220813_6W_BOJ-1212_Not-Done

---
+++ week06/이동관/BOJ_1212.java
@@ -0,0 +1,22 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class BOJ_1212 {
+  public static void main(String[] args) throws IOException {
+  // I/O Set-up
+    BufferedReader br =
+        new BufferedReader(new InputStreamReader(System.in));
+    BufferedWriter bw =
+        new BufferedWriter(new OutputStreamWriter(System.out));
+
+  // Var-init
+
+  // Calculate
+
+  // Out-put
+
+
+    bw.close();
+    br.close();
+  }
+}
