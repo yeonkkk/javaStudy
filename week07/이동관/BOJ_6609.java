16b3253181c75d84515ddff81d7529f6289f9b0d
devEdok13 <dev.edok13@gmail.com>
2022-08-23 11:36:51 +0900

    220823_7W_BOJ-2562,6609,8958_Done

--- week07/이동관/BOJ_6609.java
+++ week07/이동관/BOJ_6609.java
@@ -19,23 +19,38 @@ public class BOJ_6609 {
     long S;
     long N;
 
-  // Calculate
+  // Loop-Control
     String temp;
     while((temp = br.readLine()) != null && !temp.isEmpty()){
       st = new StringTokenizer(temp);
-      M = Long.parseLong(st.nextToken());
-      P = Long.parseLong(st.nextToken());
-      L = Long.parseLong(st.nextToken());
-      E = Long.parseLong(st.nextToken());
-      R = Long.parseLong(st.nextToken());
-      S = Long.parseLong(st.nextToken());
-      N = Long.parseLong(st.nextToken());
+      M = Long.parseLong(st.nextToken());   //  성충 초기값
+      P = Long.parseLong(st.nextToken());   //  번데기 초기값
+      L = Long.parseLong(st.nextToken());   //  유충 초기값
+      E = Long.parseLong(st.nextToken());   //  성충 모기가 알을 낳는 개수
+      R = Long.parseLong(st.nextToken());   //  R번째 유충 -> 번데기
+      S = Long.parseLong(st.nextToken());   //  S번째 번데기 -> 성충
+      N = Long.parseLong(st.nextToken());   //  기간
+      bw.write(calculate(M,P,L,E,R,S,N));
+      bw.newLine();
     }
 
   // Out-put
-
-
     bw.close();
     br.close();
+  }
+  // Calculate
+  public static String calculate(long M, long P, long L, long E, long R, long S, long N){
+
+    while(N > 0){
+      long tempM = P / S;
+      long tempP = L / R;
+      long tempL = M * E;
+      P = tempP;
+      L = tempL;
+      M = tempM;
+      N--;
+    }
+
+    return String.valueOf(M);
   }
 }
