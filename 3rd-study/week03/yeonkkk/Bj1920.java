a8b09df8434f6a06f6a35e6b9c4bea84fc6c8543
yeonkkk <tjddus0302@gmail.com>
2022-11-28 15:56:28 +0900

    yeonkkk:Boj1920

---
+++ 3rd-study/week03/yeonkkk/Bj1920.java
@@ -0,0 +1,53 @@
+/**
+ * BOJ 1920 수 찾기
+ * 1. List contains 사용하여 sequential search - 시간초과
+ * 2. 이진탐색(binary search)
+ */
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+
+public class Bj1920 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int[] arr = Arrays.stream(br.readLine().split(" "))
+                .mapToInt(Integer::parseInt)
+                .sorted()
+                .toArray();
+
+        int m = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        for (int i = 0; i < m; i++) {
+            int target = Integer.parseInt(st.nextToken());
+            System.out.println(binarySearch(arr, target));
+        }
+    }
+
+    public static int binarySearch(int[] arr, int target) {
+        int left = 0;
+        int right = arr.length - 1;
+
+        while (left <= right) {
+            int mid = (left + right) / 2;
+            int midValue = arr[mid];
+
+            if (arr[mid] < target) {
+                left = mid + 1;
+            } else if (target < midValue) {
+                right = mid - 1;
+            } else {
+                return 1;
+            }
+        }
+        return 0;
+    }
+}
+
+
+
