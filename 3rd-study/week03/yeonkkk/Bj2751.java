b14c85fc643393b7347abf5ccacf6800eb36f948
yeonkkk <tjddus0302@gmail.com>
2022-11-28 17:52:35 +0900

    yeonkkk:Boj2751

---
+++ 3rd-study/week03/yeonkkk/Bj2751.java
@@ -0,0 +1,28 @@
+/**
+ * BOJ 2751 수 정렬하기 2
+ */
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class Bj2751 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int n = Integer.parseInt(br.readLine());
+        List<Integer> nums = new ArrayList<>();
+
+        for (int i = 0; i < n; i++) {
+            nums.add(Integer.parseInt(br.readLine()));
+        }
+
+        Collections.sort(nums);
+
+        for (Integer num : nums) {
+            bw.write(num + "\n");
+        }
+        bw.close();
+    }
+}
